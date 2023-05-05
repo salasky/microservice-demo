c3b1db35f55643332cdcc8847119b0a1f7f51f2e
Мигранов Салават Вагизович <SMigranov@citros.ru>
2023-05-05 15:29:34 +0500

    Добавлен второй клиент

---
+++ eureca-client-2/src/main/java/ru/salavat/eurecaclient2/EurecaClient2Application.java
@@ -0,0 +1,13 @@
+package ru.salavat.eurecaclient2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurecaClient2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurecaClient2Application.class, args);
+    }
+
+}
---
+++ eureca-client-2/src/test/java/ru/salavat/eurecaclient2/EurecaClient2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.salavat.eurecaclient2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurecaClient2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
