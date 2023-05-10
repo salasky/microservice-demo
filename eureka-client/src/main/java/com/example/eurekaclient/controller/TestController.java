b9113fce11fbcd8886b758aff13472db382ce9b8
Salavat <migranov77@gmail.com>
2023-05-10 09:32:13 +0500

    Дабавлен eureka server ,два сервиса и gateway

--- eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/main")
 public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public ResponseEntity<String> getTestString(){
-        return ResponseEntity.ok("Test");
+        return ResponseEntity.ok(id);
     }
 }
